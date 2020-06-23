3e9e7c2a1c47869e0db35b8024e89574dd702be6
sashitaghimire <ghimiresashita@gmail.com>
2020-06-23 20:19:16 +0545

    create custom widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'resuable_card.dart';
 
 const bottomHeightContainer = 80.0;
 const activeCardColor = Color(0xFF35375f);
@@ -65,46 +67,5 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class IconContent extends StatelessWidget {
-  IconContent({this.icon,this.label});
 
-  final IconData icon;
-  final String label;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Icon(
-        icon,
-          size: 80.0,
 
-        ),
-        SizedBox(
-          height: 15.0,
-        ),
-        Text(label,style: TextStyle(
-          fontSize: 18.0, color: Color(0xFF8D8E98),
-        ),)
-      ],
-    );
-  }
-}
-
-class ResuableCard extends StatelessWidget {
-  ResuableCard({@required this.colour, this.cardChild});
-  final Color colour ;
-  final Widget cardChild;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child:cardChild,
-    margin: EdgeInsets.all(15.0),
-    decoration: BoxDecoration(
-      color:colour,
-      borderRadius: BorderRadius.circular(10.0),
-    ),
-    );
-  }
-}
